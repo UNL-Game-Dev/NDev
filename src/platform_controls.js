27dff714bcf0d8960bda2a255f68c5b5f5dfd653
31 <Durnus@gmail.com>
2013-10-30 18:32:48 -0500

    Go all the way with the groundstick method. Problems with it, as expected.

--- src/platform_controls.js
+++ src/platform_controls.js
@@ -8,6 +8,13 @@ Crafty.c("PlatformControls", {
 	init:
 	function() {
 		this.grounded = false;
+
+		this._sensor = Crafty.e("2D");
+		this._sensor.w = this.w - 2;
+		// This affects how far down the platforming physics will detect a
+		// slope to stick to.
+		this._sensor.h = 18;
+		this._sensor.addComponent("Collision");
 		
 		this.bind("PrePhysicsTick", function() {
 			// The key "x" target difference.
@@ -18,11 +25,20 @@ Crafty.c("PlatformControls", {
 			// Set the physics velocity.
 			this._phX = this._phPX + kx * 2.8;
 
+			var lastGrounded = this.grounded;
 			this.grounded = false;
 			// Search through all normals for a ground normal.
 			for(var i = this.currentNormals.length - 1; i >= 0; --i) {
-				if(dot(this.currentNormals[i], [0,-1]) > 0) {
+				var n = norm(this.currentNormals[i]);
+				if(dot(n, [0,-1]) > 0) {
 					this.grounded = true;
+					var d = dot(n,
+						[this._phX - this._phPX, this._phY - this._phPY]);
+					// Don't quite fully extract from collision.
+					// Want to keep grounded!
+					d *= 0.999;
+					this._phX -= n[0] * d;
+					this._phY -= n[1] * d;
 				}
 			}
 
@@ -30,12 +46,18 @@ Crafty.c("PlatformControls", {
 			if(this.grounded && Crafty.keydown[Crafty.keys.UP_ARROW]) {
 				this._phY = this._phPY - 5;
 				this.grounded = false;
+				// Don't try to stick.
+				lastGrounded = false;
 			}
 
 			// If not grounded, apply gravity.
 			if(!this.grounded) {
-				console.log("grav");
 				this._phAY += 280;
+			}
+
+			// If the player has left the ground, try to stick to the ground.
+			if(!this.grounded && lastGrounded) {
+				this._groundStick();
 			}
 
 		}).bind("EvaluateInertia", function() {
@@ -49,7 +71,7 @@ Crafty.c("PlatformControls", {
 				this._phY += 0.01;
 				
 			} else {
-				// If in the air, use normal intertial physics.
+				// If in the air, use normal inertial physics.
 				var px = this._phPX;
 				var py = this._phPY;
 				this._phPX = this._phX;
@@ -58,6 +80,32 @@ Crafty.c("PlatformControls", {
 				this._phY += this._phY - py;
 			}
 		});
-	}
+	},
 
+	_groundStick:
+	function() {
+		// Update position of the ground detector.
+		this._sensor.x = this._phX + 1;
+		this._sensor.y = this._phY + this.h;
+		// Find ground normals beneath the player.
+		var sensorHits = this._sensor.hit("Tile");
+		// For now, apply the first found sensor hit.
+		if(sensorHits.length) {
+			var sensorHit = sensorHits[0];
+			var n = [sensorHit.normal.x, sensorHit.normal.y];
+			if(dot(n, [0,-1]) > 0) {
+				// This is a upwards-facing slope, ok!
+
+				// Find the component of current velocity in the direction
+				// of the normal.
+				var d = dot(n, [this._phX - this._phPX, 0]);
+				// Remove that component from the velocity, so that the player
+				// slides along this surface.
+				this._phX -= n[0] * d;
+				this._phY -= n[1] * d;
+
+				console.log("Pulling towards", sensorHit.normal);
+			}
+		}
+	}
 });
