4415255593726d62a1ad9c1ffb1790258baf04f5
Sam Adams <samadams3@me.com>
2014-01-23 10:56:27 -0600

    changed parallax event from UpdateDraw to ViewportScroll

--- src/parallax.js
+++ src/parallax.js
@@ -7,7 +7,7 @@ Crafty.c("Parallax", {
 
 	init:
 	function() {
-		this.bind("UpdateDraw", function() {
+		this.bind("ViewportScroll", function() {
 			// Oppose the viewport movement.
 			this.x = -Crafty.viewport.x * this.factor;
 			this.y = -Crafty.viewport.y * this.factor;
