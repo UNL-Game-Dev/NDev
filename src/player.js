57044a756afb870c0037b4db499fe35d0bc776b0
Sam Adams <samadams3@me.com>
2014-07-09 00:58:12 -0700

    added empty item entities being loaded on init, added equip control, added handlers for action and equip controls

--- src/player.js
+++ src/player.js
@@ -10,6 +10,12 @@ Crafty.c("Player", {
     
 	init:
 	function() {
+		
+		this._pickupState = Crafty("PickupState");
+		this._items = [];
+		this._equippedItemIndex = -1;
+		this._loadItems();
+		
 		this
 		// Base components
 			.requires("2D")
@@ -91,6 +97,14 @@ Crafty.c("Player", {
 				}
 				this.isCrouching = true;
 			})
+		// Key handler
+			.bind("ControlPressed", function(ev) {
+				if(ev.control === "equip") {
+					this._equipItem();
+				} else if(ev.control === "action") {
+					this._action();
+				}
+			})
 		// Player attributes
 			.attr({
 				// Time to recover from being hit, in seconds.
@@ -112,6 +126,35 @@ Crafty.c("Player", {
 		});
         
 		this.makeScrollTarget();
+	},
+	
+	_loadItems:
+	function() {
+		var itemsDict = {
+			pistol: "Pistol",
+			dynamite: "Dynamite"
+		};
+		_(itemsDict).each(function(item, pickup) {
+			if(this._pickupState.hasPickup(pickup)) {
+				var newItem = Crafty.e(item);
+				this._items.push(newItem);
+			}
+		});
+	},
+	
+	_equipItem:
+	function() {
+		this._currentItemIndex =
+		    (this._currentItemIndex + 1) % (this._items.length + 1) - 1;
+		this._items[this._currentItemIndex].equip();
+	},
+	
+	_action:
+	function() {
+		if(this._currentItemIndex >= 0) {
+			var item = this._items[this._currentItemIndex];
+			item.activate();
+		}
 	},
 	
 	_setCollisionNormal:
