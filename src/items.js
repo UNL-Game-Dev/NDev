98fe0b603b7c86c98b8770b6bc5db17a0642f37f
Sam Adams <samadams3@me.com>
2014-07-12 02:02:18 -0700

    combined all items into one js file

--- src/items.js
+++ src/items.js
@@ -18,3 +18,24 @@ Crafty.c("Dynamite", {
 		});
 	}
 });
+
+/**
+ * Pistol item.
+ */
+Crafty.c("Pistol", {
+	init:
+	function() {
+		this.bind("ItemEquipped", function() {
+			console.log("Pistol: I was equipped!");
+		});
+		this.bind("ItemUnequipped", function() {
+			console.log("Pistol: I was unequipped!");
+		});
+		this.bind("ItemActivated", function() {
+			console.log("Pistol: I was activated!");
+		});
+		this.bind("ItemDeactivated", function() {
+			console.log("Pistol: I was deactivated!");
+		});
+	}
+});
