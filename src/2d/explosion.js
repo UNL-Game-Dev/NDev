f1442c9f4200a51ed1b548dcbe974d2a004b3ddb
Sam Adams <samadams532@gmail.com>
2015-06-06 13:54:39 -0700

    made explosion a lot more explosiony by using cc0 art

--- src/2d/explosion.js
+++ src/2d/explosion.js
@@ -12,15 +12,12 @@ Crafty.c("Explosion", {
 		this.strength = 0.5;
 		
 		// Duration of the explosion, in seconds.
-		this.time = 0.3;
+		this.time = 0.5;
 		
 		// Size of the explosion, in pixels.
 		this.size = 200;
 		
-		this.requires("2D, Canvas, Color, Tween, Delay, Collision, Hazard," +
-					  "Destructive")
-			.attr({ w: 0, h: 0 })
-			.color("#ff8800");
+		this.requires("2D, Canvas, Tween, Delay, Collision, Hazard, Destructive, SpriteAnimation, explosion");
 	},
 	
 	/**
@@ -28,18 +25,12 @@ Crafty.c("Explosion", {
 	 */
 	explode:
 	function() {
-		this.attr({
-				w: 0,
-				h: 0,
-				alpha: 1
-			})
-			.tween({
-				x: this.x - this.size / 2,
-				y: this.y - this.size / 2,
-				w: this.size,
-				h: this.size,
-				alpha: 0
-			}, this.time * 1000)
+		this
+            .attr({
+                x: this.x - 45,
+                y: this.y - 45
+            })
+            .animate('explosion')
 			.delay(this.destroy, this.time * 1000)
 			.onHit("Enemy", function(hits) {
 				_(hits).each(function(hit) {
@@ -48,7 +39,6 @@ Crafty.c("Explosion", {
 			})
 			.onHit("Physical", function(hits) {
 				_(hits).each(function(hit) {
-					var ob = hit.obj;
 					var ob = hit.obj;
 					var px = ob.x - (this.x + this.w / 2),
 						py = ob.y - (this.y + this.h / 2);
